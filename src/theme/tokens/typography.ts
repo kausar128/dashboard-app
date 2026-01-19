68fc6b970ede80559dcfdb371b3057fba0718e66
Shaikh <2397630@cognizant.com>
2026-01-19 15:28:08 +0530

    Added typography tokens, global access to tokens, ThemeShowcase page

---
+++ src/theme/tokens/typography.ts
@@ -0,0 +1,17 @@
+export const typographyTokens = {
+    fontFamily: '"Public Sans", "Inter", "Roboto", "Helvetica", sans-serif',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+    },
+    sizes: {
+      h1: '2.5rem',      // 40px
+      h2: '2rem',        // 32px
+      h6: '1.25rem',     // 20px
+      body1: '1rem',     // 16px
+      body2: '0.875rem', // 14px (often used for data tables)
+      caption: '0.75rem', // 12px
+    }
+  };
